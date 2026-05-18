e8016f86d4a175eff2d923715b0885f7a348e15b
aliaskar <lyasskar@gmail.com>
2026-05-18 18:55:19 +0500

    Fix poster asset paths for Pages

--- src/components/Decorations/FilmPosters.jsx
+++ src/components/Decorations/FilmPosters.jsx
@@ -1,9 +1,9 @@
 const POSTERS = [
-  { title: "AMÉLIE",    year: "2001", img: "/222.com/posters/amelie-print.png",
+  { title: "AMÉLIE",    year: "2001", img: "posters/amelie-print.png",
     style: { left: "2vw", top: "13vh", width: "10vh", height: "14vh", transform: "rotate(-6deg)" } },
-  { title: "BLACK MIRROR", year: "2011", img: "/222.com/posters/black-mirror-print.png",
+  { title: "BLACK MIRROR", year: "2011", img: "posters/black-mirror-print.png",
     style: { left: "2vw", top: "20vh", width: "10vh", height: "14vh", transform: "rotate(3deg)" } },
-  { title: "THE PERKS OF BEING A WALLFLOWER", year: "2012", img: "/222.com/posters/wallflower-print.png",
+  { title: "THE PERKS OF BEING A WALLFLOWER", year: "2012", img: "posters/wallflower-print.png",
     style: { right: "2vw", top: "27vh", width: "10vh", height: "14vh", transform: "rotate(5deg)" } },
 ];
 
